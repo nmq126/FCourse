df4ee1e945dbcb7d70c9f49354bf3ac2f4d0ff2b
nmq126 <minhquangngo.98@gmail.com>
2022-05-27 20:17:23 +0700

    push

---
+++ FCourse/Data/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FCourse.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+            : base("name=DBContext")
+        {
+        }
+    }
+}
