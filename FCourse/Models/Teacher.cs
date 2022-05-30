a159a457e64c04ec8da447fd7b78a9f31c772ad0
anh s <jimmianh1807@gmail.com>
2022-05-30 21:22:09 +0700

    Update database

---
+++ FCourse/Models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FCourse.Models
+{
+    public class Teacher
+    {
+        [Key]
+        [StringLength(10)]
+        public String Id { get; set; }
+        [StringLength(50)]
+        public String Name { get; set; }
+        [Column(TypeName = "ntext")]
+        public String Thumbnail { get; set; }
+        [Column(TypeName = "ntext")]
+        public String Description { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
