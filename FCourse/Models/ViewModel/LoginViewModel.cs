424d9cec5ea4ca1ca7614c9fd2daeb6b0cf7bd95
anh s <jimmianh1807@gmail.com>
2022-06-03 23:53:51 +0700

    update register Login

---
+++ FCourse/Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FCourse.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        [StringLength(25)]
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ FCourse/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FCourse.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+
+        [StringLength(25)]
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [StringLength(25)]
+        [Required]
+        public string FirstName { get; set; }
+        [StringLength(25)]
+        [Required]
+        public string LastName { get; set; }
+        [Column(TypeName = "ntext")]
+        [Required]
+        public string Description { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+    }
+}
